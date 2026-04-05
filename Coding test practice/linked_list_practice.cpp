c6813e7014e0a77c44caf5cc04774c4644170d78
L2JASON <jason0607n@naver.com>
2026-04-05 22:00:48 +0900

    make linked list class

--- Coding test practice/linked_list_practice.cpp
+++ Coding test practice/linked_list_practice.cpp
@@ -6,43 +6,55 @@ public:
     string name;
     node* link;
 };
-node* add_to_head(node* head,string new_name) {
+class LinkedList {
+private:
+    node* head;
+public:
+    LinkedList() { head = NULL; }
+    void add_to_head(string new_name);
+    void add_to_tail(string new_name);
+    void remove_node(string target);
+    void reverse_list();
+    void show_all();
+};
+void LinkedList::add_to_head(string new_name) {
     node* tmp = new node;
     tmp->name = new_name;
     tmp->link = head;
-    return tmp;
+    head = tmp;
 }
-node* add_to_tail(node* head, string new_name) {
+void LinkedList::add_to_tail(string new_name) {
     node* tmp = new node;
     tmp->name = new_name;
     tmp->link = NULL;
-    if (head == NULL) return tmp;
+    if (head == NULL) {
+        head = tmp;
+        return;
+    }
     node* p = head;
     while (p->link!=NULL) {
         p = p->link;
     }
     p->link = tmp;
-    return head;
 }
-node* remove_node(node* head,string target_name) {
+void LinkedList::remove_node(string target_name) {
     node* current = head;
     node* prev = NULL;
-    if (head == NULL) return head;
+    if (head == NULL) return;
     if (head->name == target_name) {
         head = head->link;
         delete current;
-        return head;
+        return;
     }
     while (current != NULL && current->name != target_name) {
         prev = current;
         current = current->link;
     }
-    if (current == NULL) return head;
+    if (current == NULL) return;
     prev->link = current->link;
     delete current;
-    return head;
 }
-node* reverse_list(node* head) {
+void LinkedList::reverse_list() {
     node* current = head;
     node* prev = NULL;
     node* next_node;
@@ -52,29 +64,32 @@ node* reverse_list(node* head) {
         prev = current;
         current = next_node;
     }
-    return prev;
+    head = prev;
 }
+
+void LinkedList::show_all() {
+    for (node* p = head;p!=NULL;p=p->link) {
+        cout << p->name << '\n';
+    }
+}
+
 int main(){
-    node* head = NULL;
     string input;
+    LinkedList my_list;
     for (int i=0;i<5;i++) {
         cout << "# "<<i+1<<"/5"<<" What is your favorite color: ";
         cin >> input;
-        head = add_to_tail(head,input);
+        my_list.add_to_tail(input);
     }
-    for (node* p=head;p!=NULL;p=p->link) {
-        cout << p->name << " ";
-    }
-    cout << "\nRemove color name: ";
+    cout << "Show list after input\n";
+    my_list.show_all();
+    cout << "Remove color name:";
     cin >> input;
-    head = remove_node(head,input);
-    for (node* p=head;p!=NULL;p=p->link) {
-        cout << p->name << " ";
-    }
-    cout << "\ninverse version:\n";
-    head = reverse_list(head);
-    for (node* p=head;p!=NULL;p=p->link) {
-        cout << p->name << " ";
-    }
+    my_list.remove_node(input);
+    cout << "Show list after remove\n";
+    my_list.show_all();
+    my_list.reverse_list();
+    cout << "Show list after inverse\n";
+    my_list.show_all();
     return 0;
 }
